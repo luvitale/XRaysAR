f43876f93bff333251f99cd858110ccf68bccd8d
Luciano Nahuel Vitale <lunavi97@gmail.com>
2019-12-30 14:44:44 -0300

    Implemented flash torch button

---
+++ Assets/Scripts/FlashTorch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class FlashTorch : MonoBehaviour
+{
+    public void SetFlashTorch(bool enabled)
+    {
+        Debug.Log($"FlashTorch: {enabled}");
+        CameraDevice.Instance.SetFlashTorchMode(enabled);
+    } 
+}
